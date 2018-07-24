ef5ab4a43918b5ea3a0a66b18b72c07a5d94c1ab
Kathleen Vincent <kvincen4316@gmail.com>
2018-07-23 23:22:13 -0500

    friend finder

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# friendfinder
-friendfinder
+# Friend Finder Application
+
+## Description
+**Friend Finder** implements friend matching based on the user's responses to a ten question survey. The user responds to questions with values from 1 (Strongly Disagree) to 5 (Strongly Agree). When the survey is submitted, an existing user record closest to the current user's responses is found and returned. The closest set of user responses is defined as the set with the lowest absolute difference for all ten questions combined.
+
+**Friend Finder** application is meant to stimulate a simple dating app. The application is implemented using a Node.js and Express server on the backend and Bootstrap CSS Framework on the frontend.
+
+## Demo
+**Friend Finder** is deployed to Heroku. Please check it out [here] ().
+
+## Installation
+To install the application follow the following instruction below:
+```javascript
+git clone github.com/kmvincent/bamazon.git
+cd friend-finder
+npm install
+```
+
+## Running Locally
+To run the application locally and access it in your browser, first set the PORT environment variable to the value of your choice. An example is shown below.
+```javascript
+export PORT=3030
+```
+After the PORT environment variable has been set, run the Node.js application with the command line below.
+```javascript
+node server.js
+```
+The application will now be running locally on PORT, in this case that is port 3030. You can then access it locally from your browser at the URL localhost:PORT, in this case localhost:3030.
---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friends = [
+{
+    name: "Kathleen Vincent",
+    pic: "https://images.unsplash.com/photo-1487300001871-12053913095d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=508d6e7b47ad1b46b0ce324b2b91b708&auto=format&fit=crop&w=1950&q=80",
+    scores: [
+        "1",
+        "3", 
+        "4", 
+        "3", 
+        "5", 
+        "4", 
+        "4", 
+        "2",
+        "2", 
+        "1"
+    ]
+},
+{
+    name: "Neighborhood Cat Lady",
+    pic: "https://images.unsplash.com/photo-1523204188200-e37cd0e8cfc6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=25ad3e14a5bdae8f27e74b48fe70b837&auto=format&fit=crop&w=1950&q=80",
+    scores: [
+        "5",
+        "1", 
+        "1", 
+        "1", 
+        "1", 
+        "5", 
+        "3", 
+        "4",
+        "3", 
+        "1"
+    ]
+},
+{
+    name: "The Vigilante",
+    pic: "https://images.unsplash.com/photo-1522032999890-bac6832444c6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4dbc586f4e4439ac65e39764b91f50c1&auto=format&fit=crop&w=1950&q=80",
+    scores: [
+        "5",
+        "1", 
+        "1", 
+        "1", 
+        "1", 
+        "5", 
+        "5", 
+        "2",
+        "1", 
+        "4"
+    ]
+},
+{
+    name: "The Creepy Neighbor",
+    pic: "https://images.unsplash.com/photo-1481018085669-2bc6e4f00eed?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a1acecf5912c7c8fad7b479e6bfc653c&auto=format&fit=crop&w=1950&q=80",
+    scores: [
+        "5",
+        "5", 
+        "5", 
+        "5", 
+        "1", 
+        "5", 
+        "1", 
+        "1",
+        "1", 
+        "1"
+    ]
+},
+];
+
+module.exports = friends;
